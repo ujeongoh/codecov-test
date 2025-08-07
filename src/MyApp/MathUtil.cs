df60ef86591666925e8da7ee0472e607782428fb
Joy Oh <yOh2@nowcom.com>
2025-08-07 14:43:24 +0900

    add fuctions

--- src/MyApp/MathUtil.cs
+++ src/MyApp/MathUtil.cs
@@ -7,9 +7,19 @@ namespace MyApp
             return a + b;
         }
 
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
         public static int Multiply(int a, int b)
         {
             return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
         }
     }
 }
